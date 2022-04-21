7e63be74ea2bc23386fcc6a8ca727a75064a691a
Parsa Mohammadian <parsa2820@gmail.com>
2022-04-21 22:01:36 +0430

    Start game client

--- hw1/game-client/gameclient.py
+++ hw1/game-client/gameclient.py
@@ -1,6 +1,10 @@
+import socket
+
+
 class GameClient:
-    def __init__(self, web_server_port):
+    def __init__(self, web_server_address, web_server_port):
+        self.web_server_address = web_server_address
         self.web_server_port = web_server_port
 
     def run(self):
-        pass
+        
