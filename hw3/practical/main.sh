e395d19c8a9bffd079d9bad15ed7fb0ee697d2c6
Parsa Mohammadian <parsa2820@gmail.com>
2022-07-06 09:51:12 -0400

    Add main menu items

--- hw3/practical/main.sh
+++ hw3/practical/main.sh
@@ -8,11 +8,23 @@
 
 main_menu_items=(
     "Block incoming/outgoing traffic to/from a specific IP, domain RegEx URL, or port."
-    "Hi"
+    "Block incoming/outgoing traffic base on count, protocol, and request type."
+    "Block traffic if header contains a specific string."
+    "Config DoS attack prevention."
+    "Use DB to improve DoS attack prevention."
+    "Block port scan and port knocking."
+    "Flush all rules."
+    "Hi."
 )
 
 main_menu_functions=(
-    block_traffic
+    block_traffic_ip_url_port
+    block_traffic_count_protocol_request_type
+    block_traffic_header_string
+    config_dos_attack_prevention
+    use_db_improve_dos_attack_prevention
+    block_port_scan_and_port_knocking
+    flush_all_rules
     hi
 )
 
@@ -32,6 +44,24 @@ block_traffic_menu_functions=(
     block_traffic_to_domain
     block_traffic_from_port
     block_traffic_to_port
+)
+
+block_traffic_count_protocol_request_type_menu_items=(
+    "FTP, Block specific file"
+    "SSH, Blcok specific username"
+    "DNS, Block specific domain"
+    "DHCP, Block specific MAC address"
+    "HTTP and HTTPS, Block specific content"
+    "SMTP, Block specific email address"
+)
+
+block_traffic_count_protocol_request_type_menu_functions=(
+    block_traffic_count_protocol_request_type_ftp
+    block_traffic_count_protocol_request_type_ssh
+    block_traffic_count_protocol_request_type_dns
+    block_traffic_count_protocol_request_type_dhcp
+    block_traffic_count_protocol_request_type_http_https
+    block_traffic_count_protocol_request_type_smtp
 )
 
 
@@ -62,12 +92,85 @@ echo_menu() {
 }
 
 #######################################
+# Get traffic type from user.
+# Returns:
+#   Traffic type (INPUT, OUTPUT, FORWARD), a string.
+#######################################
+ask_traffic_type() {
+    echo "Enter traffic type(incoming[i], outgoing[o], forward[f]): "
+    read -r traffic_type
+    case $traffic_type in
+        i)
+            return "INPUT"
+            ;;
+        o)
+            return "OUTPUT"
+            ;;
+        f)
+            return "FORWARD"
+            ;;
+        *)
+            echo "Invalid traffic type."
+            exit 1
+            ;;
+    esac
+}
+
+#######################################
 # Block incoming/outgoing traffic to/from a specific IP, domain RegEx URL, or port.
 #######################################
-block_traffic() {
+block_traffic_ip_url_port() {
     clear
     echo_menu "${block_traffic_menu_items[@]}"
     ${block_traffic_menu_functions[ $(( $? - 1 )) ]}
+}
+
+#######################################
+# Block incoming/outgoing traffic base on count, protocol, and request type.
+#######################################
+block_traffic_count_protocol_request_type() {
+    clear
+    echo_menu "${block_traffic_count_protocol_request_type_menu_items[@]}"
+    ${block_traffic_count_protocol_request_type_menu_functions[ $(( $? - 1 )) ]}
+}
+
+#######################################
+# Block traffic if header contains a specific string.
+#######################################
+block_traffic_header_string() {
+    clear 
+}
+
+#######################################
+# Config DoS attack prevention.
+#######################################
+config_dos_attack_prevention() {
+    clear
+}
+
+#######################################
+# Use DB to improve DoS attack prevention.
+#######################################
+use_db_improve_dos_attack_prevention() {
+    clear
+}
+
+#######################################
+# Block port scan and port knocking.
+#######################################
+block_port_scan_and_port_knocking() {
+    clear
+}
+
+#######################################
+# Flush all rules.
+#######################################
+flush_all_rules() {
+    echo "Flushing all rules..."
+    iptables --flush
+    iptables -P INPUT ACCEPT
+    iptables -P OUTPUT ACCEPT
+    iptables -P FORWARD ACCEPT
 }
 
 #######################################
@@ -98,6 +201,15 @@ block_traffic_from_domain() {
 }
 
 #######################################
+# Block specfic file in FTP protocol
+#######################################
+block_traffic_count_protocol_request_type_ftp() {
+    echo -n "Enter the file to block: "
+    read -r file
+    iptables -A INPUT -p tcp --dport 21 -m string --string $file --algo bm -j DROP
+}
+
+#######################################
 # Hi
 #######################################
 hi() {
@@ -105,13 +217,25 @@ hi() {
 }
 
 #######################################
-# Script starting point.
+# Show main menu.
 #######################################
-main() {
+main_menu() {
     clear
     echo_menu "${main_menu_items[@]}"
     ${main_menu_functions[ $(( $? - 1 )) ]}
 }
 
+#######################################
+# Script starting point.
+# Loop on main menu until user exits.
+#######################################
+main() {
+    while true; do
+        main_menu
+        sleep_time=3
+        echo "Showing main menu in $sleep_time seconds..."
+        sleep $sleep_time
+    done
+}
 
-main $@
+main
